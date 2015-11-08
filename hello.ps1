e4f140a0a61be7d2ac5bdf7f38895fe3d0d5ccff
coops07 <coops_2121@Hotmail.com>
2015-11-08 14:19:58 -0500

    first lesson - scripts
    testing out cmdlets and syntax from first lesson

---
+++ hello.ps1
@@ -0,0 +1,27 @@
+echo "Hello world!"
+echo "enter your data into array - push enter after each, and press enter again when done"
+$var1 = echo 
+$var1
+echo "this is [0]"
+$var1[0]
+echo "this is [1]"
+$var1[1]
+echo "this is [2]"
+$var1[2]
+echo "this is [3]"
+$var1[3]
+echo "this is [4]"
+$var1[4]
+
+echo "This is my second script script..! "
+echo "@"
+echo "#"
+echo "%"
+echo "^"
+echo "&"
+echo "*"
+echo "("
+echo ")"
+echo "_"
+echo "+"
+echo "test complete."
